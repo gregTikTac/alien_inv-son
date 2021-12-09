7f1606cad8c74e8c38419a00aa1b2924ca15f14d
root <skyfallagain009@gmail.com>
2021-12-09 13:28:14 -0500

    class alien and def init

---
+++ alien_invasion.py
@@ -0,0 +1,10 @@
+import sys
+
+import pygame
+
+class AlienInvision:
+    """class for control resources and game behavior"""
+
+    def __init__(self):
+        """init game and create game resources"""
+        pygame.init()
