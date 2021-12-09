6d637d0e2a6c168497f5c9a7be18c226784a2c4a
root <skyfallagain009@gmail.com>
2021-12-09 13:59:08 -0500

    diplay.flip for update window run_geme for when calling the function directly

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import sys  # ends game on commands player
 import pygame  # contains functional
 
 
-class AlienInvision:
+class AlienInvasion:
     """class for control resources and game behavior"""
 
     def __init__(self):
@@ -16,6 +16,15 @@ class AlienInvision:
     def run_game(self):
         """start loop_game"""
         while True:     # event loop
-            for event in pygame.event.get():  # tracking keyboard and mouse events
+            for event in pygame.event.get():  # tracking keyboard and mouse events (pygame.event == access for events)
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            """display the last drawn screen"""
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    """create instance and start game"""
+    ai = AlienInvasion()
+    ai.run_game()
