5f4e79f34713f57163d8c1192f3c23276c0d66ae
root <skyfallagain009@gmail.com>
2021-12-10 14:15:37 -0500

    diplay param

---
+++ setting.py
@@ -0,0 +1,9 @@
+class Setting():
+    """class for storing setting game Alien_ Invasion"""
+
+    def __init__(self):
+        """init setting game"""
+        # display parameters
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (150, 200, 2)
