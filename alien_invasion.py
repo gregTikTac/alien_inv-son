5e153df458a662f2c71eb503f237f05c935d175b
root <skyfallagain009@gmail.com>
2021-12-10 14:07:02 -0500

    bg_color fill(bg_color)

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,4 @@
 import sys  # ends game on commands player
-
 import pygame  # contains functional
 
 
@@ -12,14 +11,16 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))  # call display (self.screen == surface)
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (150, 200, 2)  # set window color
 
     def run_game(self):
         """start loop_game"""
-        while True:     # event loop
+        while True:  # event loop
             for event in pygame.event.get():  # tracking keyboard and mouse events (pygame.event == access for events)
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.bg_color)  # on each pass of the loop, color is drawn
             """display the last drawn screen"""
             pygame.display.flip()
 
