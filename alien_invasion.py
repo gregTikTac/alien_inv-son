5aad6c263c455fc051a6b460609df85e5bbff53a
root <skyfallagain009@gmail.com>
2021-12-10 14:21:28 -0500

    instance setting display mode setting.b_collor

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys  # ends game on commands player
 import pygame  # contains functional
+from setting import Setting
 
 
 class AlienInvasion:
@@ -8,7 +9,9 @@ class AlienInvasion:
     def __init__(self):
         """init game and create game resources"""
         pygame.init()
+        self.setting = Setting()  # instance class Setting
 
+        self.screen = pygame.display.set_mode((self.setting.screen_width, self.setting.screen_height))
         self.screen = pygame.display.set_mode((1200, 800))  # call display (self.screen == surface)
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (150, 200, 2)  # set window color
@@ -20,7 +23,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)  # on each pass of the loop, color is drawn
+            self.screen.fill(self.setting.bg_color)  # on each pass of the loop, color is drawn
             """display the last drawn screen"""
             pygame.display.flip()
 
