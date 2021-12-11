3f4583f56d173f4ab43e6d62b027d71572099709
root <skyfallagain009@gmail.com>
2021-12-11 02:07:56 -0500

    refactoring def run_game: add _check_events

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,10 +19,15 @@ class AlienInvasion:
 
     def run_game(self):
         """start loop_game"""
-        while True:  # event loop
-            for event in pygame.event.get():  # tracking keyboard and mouse events (pygame.event == access for events)
-                if event.type == pygame.QUIT:
-                    sys.exit()
+        while True:
+            self._chek_events()  # call method
+
+            #  draw window
+    def _check_events(self):
+        """handles keystrokes and mouse events"""
+        for event in pygame.event.get():  # tracking keyboard and mouse events (pygame.event == access for events)
+            if event.type == pygame.QUIT:
+                sys.exit()
 
             self.screen.fill(self.setting.bg_color)  # on each pass of the loop, color is drawn
             self.ship.blitme()  # draw ship in window
