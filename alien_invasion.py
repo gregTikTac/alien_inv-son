5761efc02bbcc38c4290bbb86b4dd51cf926afc3
root <skyfallagain009@gmail.com>
2021-12-12 08:35:34 -0500

    'q' for completion

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,6 +41,8 @@ class AlienInvasion:
             self.ship.moving_right = True  # move ship right (if push bottom "->")
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         "Reacts when keys are released"
