1c5bd90c60fa0d764dd6033dd24d7089f473869a
root <skyfallagain009@gmail.com>
2021-12-12 06:12:00 -0500

    + ship_speed

--- setting.py
+++ setting.py
@@ -7,3 +7,7 @@ class Setting():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        """settings ship"""
+        self.ship_speed = 1.5  # pixel ship = 1.5
+
