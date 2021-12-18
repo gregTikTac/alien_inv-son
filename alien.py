7d75b205ad8410b3d97e7db925d2025ebc771f2a
root <skyfallagain009@gmail.com>
2021-12-18 14:08:49 -0500

    + positon for alien

---
+++ alien.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """class for one alien"""
+
+    def __init__(self, ai_game):
+        """init alien and set him start position"""
+        super().__init__()
+        self.screen = ai_game.screen
+
+        self.image = pygame.image.load("images/ufo.bmp")
+        self.rect = self.image.get_rect()
+
+        # everyone alian starting in the left-up coal
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # saved alien position
+        self.x = float(self.rect.x)
